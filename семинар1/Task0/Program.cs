f284247d7181096a90009b4e0d389e3a219bb129
iklove2 <iklove@yandex.ru>
2022-06-23 20:36:32 +0300

    первый коммит

---
+++ семинар1/Task0/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Находим квадрат введенного числа
+*/
+
+Console.Write("Введите число для возведения в квадрат: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int squareOfNumber = number * number; 
+Console.WriteLine($"Квадрат числа {number} равен {squareOfNumber}");
