6bb4f4c9c9dca32814bd94c1e0bdcd6670a9bd8d
iklove2 <iklove@yandex.ru>
2022-06-23 21:04:30 +0300

    Task3

---
+++ семинар1/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int dayweek = Convert.ToInt32(Console.ReadLine());
+switch (dayweek)  
+{
+    case 1:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} понедельник");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} вторник");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} среда");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} четверг");
+            break;
+        }
+    case 5:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} пятница");
+            break;
+        }
+    case 6:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} суббота");
+            break;
+        }
+    case 7:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} воскресенье");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine($"День недели для номера дня {dayweek} не существует");
+            break;
+        }
+
+}
